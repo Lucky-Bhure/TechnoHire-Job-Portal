fc167c14d3d731308119b84ecfc5e226fa66e0dd
Anuja Srivastava <anujasrivastava51@gmail.com>
2024-12-07 13:07:36 +0530

    HomePge

--- src/pages/modules/Homepage/Homepage.jsx
+++ src/pages/modules/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Illustration from '../../../assets/Illustration.png';
 import FbLogo from '../../../assets/FbLogo.png';
 import UpLogo from '../../../assets/UpLogo.png';
+import ULogo from '../../../assets/ULogo.png';
 import appleLogo from '../../../assets/appleLogo.png';
 import GoogleLogo from '../../../assets/GoogleLogo.png';
 import EmployersLogo from '../../../assets/EmployersLogo.png';
@@ -9,18 +10,21 @@ import EmployersLogo from '../../../assets/EmployersLogo.png';
 
 import { FaBandAid, FaSearch } from "react-icons/fa";
 import { FaBriefcase } from 'react-icons/fa';
-import { FaBuilding } from 'react-icons/fa';
-import { FaUser } from 'react-icons/fa';
+import { PiBuildingOffice } from 'react-icons/pi';
+import { FaUserPlus } from "react-icons/fa";
+import { FaUsers } from 'react-icons/fa';
 import { FaCloudUploadAlt } from 'react-icons/fa';
 import { FaSearchPlus } from 'react-icons/fa';
-import { FaCheckCircle } from 'react-icons/fa';
+import { FaRegCheckCircle } from "react-icons/fa";
 import { FaArrowRight } from 'react-icons/fa';
-import { FaPen } from 'react-icons/fa';
+import { FaArrowLeft } from 'react-icons/fa';
+import { FaPenNib } from 'react-icons/fa';
+import { LuMonitorPlay } from "react-icons/lu";
 import { FaCode } from 'react-icons/fa';
-import { FaPlay } from 'react-icons/fa';
+import { HiOutlineSpeakerphone } from "react-icons/hi";
 import { FaMusic } from 'react-icons/fa';
 import { FaChartBar } from 'react-icons/fa';
-import { FaFirstAid } from 'react-icons/fa';
+import { RiFirstAidKitLine } from 'react-icons/ri';
 import { FaDatabase } from 'react-icons/fa';
 import { FaMap } from 'react-icons/fa';
 import { FaBookmark } from 'react-icons/fa';
@@ -89,14 +93,14 @@ const Homepage = () => {
               </span>
             </div>
             <div className="flex gap-10 justify-center items-center  h-[90px] rounded-lg bg-white border shadow-md ">
-              <FaBuilding className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
+              <PiBuildingOffice className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
               <span>
                 <h5 className='font-bold text-lg'>97,345</h5>
                 <p className='text-gray-500'>Companies</p>
               </span>
             </div>
             <div className="flex gap-10 justify-center items-center  h-[90px] rounded-lg bg-white border shadow-md ">
-              <FaBriefcase className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
+              <FaUsers className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
               <span>
                 <h5 className='font-bold text-lg'>38,47,154</h5>
                 <p className='text-gray-500'>Candidates</p>
@@ -192,7 +196,7 @@ const Homepage = () => {
           <div className="grid grid-cols-1 mt-6 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-6">
 
             <div className="flex flex-col gap-6 justify-center items-center  h-[244px] w-[250px] rounded-lg">
-              <FaUser className='w-[40px] h-[40px] mt-4 text-[#6712B9] bg-white p-2 rounded-full ' />
+              <FaUserPlus className='w-[40px] h-[40px] mt-4 text-[#6712B9] bg-white p-2 rounded-full ' />
               <h5 className='font-medium text-lg'>Create Account</h5>
               <p className=' text-gray-400 text-sm p-4 align-middle'>Join us today to unlock a world of<br />
                 career possibilities. Setting up is quick
@@ -212,7 +216,7 @@ const Homepage = () => {
             </div>
 
             <div className="flex flex-col gap-6 justify-center items-center  h-[244px] w-[250px] rounded-lg">
-              <FaCheckCircle className='w-[40px] h-[40px] mt-4 text-[#6712B9] bg-white p-2 rounded-full ' />
+              <FaRegCheckCircle className='w-[40px] h-[40px] mt-4 text-[#6712B9] bg-white p-2 rounded-full ' />
               <h5 className='font-medium text-lg'>Apply Job</h5>
               <p className=' text-gray-400 text-sm p-4 align-center'>Ready to take the next step? Submit your application with a click and get closer to your dream job!</p>
             </div>
@@ -232,7 +236,7 @@ const Homepage = () => {
             <div className="grid grid-cols-2  sm:grid-cols-2 lg:grid-cols-4 gap-12 p-6">
 
               <div className="flex gap-8 justify-center items-center  h-[90px] w-[260px] rounded-lg bg-white ">
-                <FaPen className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
+                <FaPenNib  className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
                 <span>
                   <h5 className='text-lg font-medium'>Graphics & Design</h5>
                   <p className='text-gray-500'>357 Open position</p>
@@ -246,7 +250,7 @@ const Homepage = () => {
                 </span>
               </div>
               <div className="flex gap-8 justify-center items-center  h-[90px] w-[260px] rounded-lg bg-white">
-                <FaCode className='text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
+                <HiOutlineSpeakerphone className='text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
                 <span>
                   <h5 className='text-lg font-medium'>Digital Marketing</h5>
                   <p className='text-gray-500'>297 Open position</p>
@@ -254,7 +258,7 @@ const Homepage = () => {
               </div>
 
               <div className="flex gap-8 justify-center items-center  h-[90px] w-[260px] rounded-lg bg-white ">
-                <FaPlay className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
+                <LuMonitorPlay className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
                 <span>
                   <h5 className='text-lg font-medium'>Video & Animation</h5>
                   <p className='text-gray-500'>247 Open position</p>
@@ -275,14 +279,14 @@ const Homepage = () => {
                 </span>
               </div>
               <div className="flex gap-8 justify-center items-center  h-[90px] w-[260px] rounded-lg bg-white">
-                <FaBuilding className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
+                <PiBuildingOffice className='text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
                 <span>
                   <h5 className='text-lg font-medium'>UI/UX Design</h5>
                   <p className='text-gray-500'>167 Open position</p>
                 </span>
               </div>
               <div className="flex gap-6 justify-center items-center  h-[90px] w-[260px] rounded-lg bg-white">
-                <FaFirstAid className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
+                <RiFirstAidKitLine className=' text-[#6712B9] bg-gray-100 w-12 p-2 h-10' />
                 <span>
                   <h5 className='text-lg font-medium'>Blockchain Technology</h5>
                   <p className='text-gray-500'>125 Open position</p>
@@ -303,11 +307,11 @@ const Homepage = () => {
           </div>
         </div>
 
-        <hr className='w-full' />
+         <hr className='w-full' /> 
 
-        <div className='h-[542px] w-full pt-10 pl-32 pr-32'>
+        <div className='h-[800px] w-full pt-10 pl-32 pr-32'>
           <h1 className='font-normal text-[35px] pb-6'>Recents Jobs</h1>
-          <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-5'>
           <div className="flex gap-10 h-[90px] rounded-lg bg-white border shadow-md ">
               <div className="flex items-start p-4">
                 <img className="h-16 w-16" src={UpLogo} alt="UP Logo" />
@@ -347,10 +351,45 @@ const Homepage = () => {
               <div className="flex items-start p-4">
                 <img className="h-16 w-16" src={appleLogo} alt="UP Logo" />
                 <div className="ml-4 flex-grow">
-                  <div className="flex gap-2 justify-between pb-2">
-                    <span className="text-xl font-semibold text-gray-800">Software Engineer</span>
+                  <div className="flex justify-between pb-2">
+                    <span className="text-xl font-semibold text-[#6712B9]">Software Engineer</span>
                     <button className="px-4 font-medium  text-sm py-1 rounded-full bg-[#F5EBFFB2] text-[#6712B9] ">
                        Full Time
+                    </button>
+                  </div>
+                  <div className="flex gap-5 justify-between">
+                    <div className="flex items-center">
+                      <FaMap className="text-gray-400 text-sm" />
+                      <span className="font-medium text-gray-400 text-sm">Mumbai</span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-gray-400 text-sm">₹</span>
+                      <span className="font-medium text-gray-400 text-sm">50k-60k</span>
+                    </div>
+                    <div className="flex items-center">
+                      <FaCalendar className="text-gray-400 text-sm" />
+                      <span className="font-medium text-gray-400 text-sm">4 days remaining</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-8 items-center  ml-[40%]">
+                <FaBookmark className='text-[#6712B9]' />
+                <button className="flex h-10 pl-4 pr-4 items-center bg-[#6712B9] text-white rounded-md">
+                  <span>View All</span>
+                  <FaArrowRight className="ml-2" />
+                </button>
+              </div>
+            </div>
+
+            <div className="flex gap-10 h-[90px] rounded-lg bg-white border shadow-md ">
+              <div className="flex items-start p-4">
+                <img className="h-16 w-16" src={EmployersLogo} alt="EmployeerLogo" />
+                <div className="ml-4 flex-grow">
+                  <div className="flex justify-between pb-2">
+                    <span className="text-xl font-semibold text-gray-800">Junior Graphic Designer</span>
+                    <button className="px-2 font-medium  text-sm py-1 rounded-full bg-[#F5EBFFB2] text-[#6712B9] ">
+                      Full Time
                     </button>
                   </div>
                   <div className="flex justify-between">
@@ -360,7 +399,7 @@ const Homepage = () => {
                     </div>
                     <div className="flex items-center">
                       <span className="text-gray-400 text-sm">₹</span>
-                      <span className="font-medium text-gray-400 text-sm">30k-35k</span>
+                      <span className="font-medium text-gray-400 text-sm">50k-70k</span>
                     </div>
                     <div className="flex items-center">
                       <FaCalendar className="text-gray-400 text-sm" />
@@ -378,6 +417,127 @@ const Homepage = () => {
               </div>
             </div>
           
+            <div className="flex gap-10 h-[90px] rounded-lg bg-white border shadow-md ">
+              <div className="flex items-start p-4">
+                <img className="h-16 w-16" src={ULogo} alt="ULogo" />
+                <div className="ml-4 flex-grow">
+                  <div className="flex gap-2 justify-between pb-2">
+                    <span className="text-xl font-semibold text-gray-800">Senior UX Designer</span>
+                    <button className="px-2 font-medium  text-sm py-1 rounded-full bg-[#F5EBFFB2] text-[#6712B9] ">
+                      Contract Base
+                    </button>
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex items-center">
+                      <FaMap className="text-gray-400 text-sm" />
+                      <span className="font-medium text-gray-400 text-sm">Pune</span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-gray-400 text-sm">₹</span>
+                      <span className="font-medium text-gray-400 text-sm">35-40k</span>
+                    </div>
+                    <div className="flex items-center">
+                      <FaCalendar className="text-gray-400 text-sm" />
+                      <span className="font-medium text-gray-400 text-sm">4 days remaining</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-8 items-center  ml-[40%]">
+                <FaBookmark className='text-[#6712B9]' />
+                <button className="flex h-10 pl-4 pr-4 items-center text-[#6712B9] rounded-md border-2">
+                  <span>View All</span>
+                  <FaArrowRight className="ml-2" />
+                </button>
+              </div>
+            </div>
+
+            <div className="flex gap-10 h-[90px] rounded-lg bg-white border shadow-md ">
+              <div className="flex items-start p-4">
+                <img className="h-16 w-16" src={FbLogo} alt="FbLogo" />
+                <div className="ml-4 flex-grow">
+                  <div className="flex  justify-between pb-2">
+                    <span className="text-xl font-semibold text-gray-800">Marketing Officer</span>
+                    <button className="px-2 font-medium  text-sm py-1 rounded-full bg-[#F5EBFFB2] text-[#6712B9] ">
+                      Internship
+                    </button>
+                  </div>
+                  <div className="flex gap-7  justify-between">
+                    <div className="flex items-center">
+                      <FaMap className="text-gray-400 text-sm" />
+                      <span className="font-medium text-gray-400 text-sm">Pune</span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-gray-400 text-sm">₹</span>
+                      <span className="font-medium text-gray-400 text-sm">50k-90k</span>
+                    </div>
+                    <div className="flex items-center">
+                      <FaCalendar className="text-gray-400 text-sm" />
+                      <span className="font-medium text-gray-400 text-sm">4 days remaining</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-8 items-center  ml-[40%]">
+                <FaBookmark className='text-[#6712B9]' />
+                <button className="flex h-10 pl-4 pr-4 items-center text-[#6712B9] rounded-md border-2">
+                  <span>View All</span>
+                  <FaArrowRight className="ml-2" />
+                </button>
+              </div>
+            </div>
+
+            <div className="flex gap-10 h-[90px] rounded-lg bg-white border shadow-md ">
+              <div className="flex items-start p-4">
+                <img className="bg-gray-300 h-16 w-16 p-3" src={GoogleLogo} alt="Google Logo" />
+                <div className="ml-4 flex-grow">
+                  <div className="flex gap-2 justify-between pb-2">
+                    <span className="text-xl font-semibold text-gray-800">Interaction Designer</span>
+                    <button className="px-2 font-medium  text-sm py-1 rounded-full bg-[#F5EBFFB2] text-[#6712B9] ">
+                     Full Time
+                    </button>
+                  </div>
+                  <div className="flex gap-6 justify-between">
+                    <div className="flex items-center">
+                      <FaMap className="text-gray-400 text-sm" />
+                      <span className="font-medium text-gray-400 text-sm">Nagpur</span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-gray-400 text-sm">₹</span>
+                      <span className="font-medium text-gray-400 text-sm">5k-10k</span>
+                    </div>
+                    <div className="flex items-center">
+                      <FaCalendar className="text-gray-400 text-sm" />
+                      <span className="font-medium text-gray-400 text-sm">4 days remaining</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-8 items-center  ml-[40%]">
+                <FaBookmark className='text-[#6712B9]' />
+                <button className="flex h-10 pl-4 pr-4 items-center text-[#6712B9] rounded-md border-2">
+                  <span>View All</span>
+                  <FaArrowRight className="ml-2" />
+                </button>
+              </div>
+            </div>
+
+          </div>
+        </div> 
+
+        <div className='h-[650px] bg-[#FFFF] w-full pl-32 pr-32 pt-10'>
+          <div className='flex justify-between'>
+            <h1 className='font-normal text-[35px] pb-6>Top Companies'>Top Companies</h1>
+
+            <div className="flex  space-x-4">
+              <button className="p-4 text-[#6712B9] bg-gray-200 rounded-md ">
+                <FaArrowLeft className="text-lg" />
+              </button>
+
+              <button className="p-4 text-[#6712B9] bg-gray-200 rounded-md">
+                <FaArrowRight className="text-lg" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
