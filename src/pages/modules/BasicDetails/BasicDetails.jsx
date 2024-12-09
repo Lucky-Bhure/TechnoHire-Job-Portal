857b47f19da866d1f5f5e61764a067e68c964792
kirtikalmegh <kalmeghkirti@gmail.com>
2024-12-10 00:14:27 +0530

    committed education, workExp, Certify changes

--- src/pages/modules/BasicDetails/BasicDetails.jsx
+++ src/pages/modules/BasicDetails/BasicDetails.jsx
@@ -7,6 +7,10 @@ import CertifyLogo from "../../../assets/img/certify.png";
 import SkillPrefLogo from "../../../assets/img/skillPref.png";
 import UploadDocLogo from "../../../assets/img/uploadDoc.png";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import EducationDetails from "./EducationDetails";
+import WorkExp from "./WorkExp";
+import Certify from "./Certify";
+import UploadDoc from "./UploadDoc";
 
 const BasicDetails = () => {
   const [currentStep, setCurrentStep] = useState(1);
@@ -22,10 +26,19 @@ const BasicDetails = () => {
 
   const steps = [
     "Basic Details",
+    "Education",
+    "Work Experience",
+    "Certification",
+    "Skills",
+    "Documents"
+  ];
+
+  const formsName = [
+    "Basic Details",
     "Education Details",
     "Work Experience",
     "Certification",
-    "Skills and Preferences",
+    "Skills and Preference",
     "Documents"
   ];
 
@@ -38,6 +51,15 @@ const BasicDetails = () => {
     UploadDocLogo
   ];
 
+  const subhead = [
+    "Let’s Start with the Basics",
+    "Your Educational Background",
+    "Tell us your Work Experience",
+    "Do you Have Certifications?",
+    "Highlight Your Skills and Preferences to stand out",
+    "Upload Supporting Documents"
+  ];
+
   const handleInputChange = (e) => {
     const { id, value } = e.target;
     setFormData({ ...formData, [id]: value });
@@ -45,16 +67,7 @@ const BasicDetails = () => {
   };
 
   const validateForm = () => {
-    const newErrors = {};
-    if (!formData.fullName) newErrors.fullName = "Full Name is required.";
-    if (!formData.dob) newErrors.dob = "Date of Birth is required.";
-    if (!formData.gender) newErrors.gender = "Gender is required.";
-    if (!formData.contact) newErrors.contact = "Contact Number is required.";
-    if (!formData.email) newErrors.email = "Email Address is required.";
-    if (!formData.location) newErrors.location = "Location is required.";
-
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
+    return true;
   };
 
   const handleSubmit = (e) => {
@@ -79,11 +92,8 @@ const BasicDetails = () => {
                 placeholder="Full Name"
                 value={formData.fullName}
                 onChange={handleInputChange}
-                className="w-full bg-[#ece9e99c] px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
+                className="w-full bg-[#ece9e99c] px-4 py-[14px] border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
               />
-              {errors.fullName && (
-                <p className="text-red-500 text-sm">{errors.fullName}</p>
-              )}
             </div>
             <div>
               <input
@@ -91,27 +101,21 @@ const BasicDetails = () => {
                 id="dob"
                 value={formData.dob}
                 onChange={handleInputChange}
-                className="w-full bg-[#ece9e99c] px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
+                className="w-full bg-[#ece9e99c] px-4 py-[14px] border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
               />
-              {errors.dob && (
-                <p className="text-red-500 text-sm">{errors.dob}</p>
-              )}
             </div>
             <div>
               <select
                 id="gender"
                 value={formData.gender}
                 onChange={handleInputChange}
-                className="w-full bg-[#ece9e99c] px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
+                className="w-full bg-[#ece9e99c] px-4 py-[14px] border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
               >
                 <option value="">Select Gender</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
-              {errors.gender && (
-                <p className="text-red-500 text-sm">{errors.gender}</p>
-              )}
             </div>
             <div>
               <input
@@ -120,11 +124,8 @@ const BasicDetails = () => {
                 placeholder="Contact Number"
                 value={formData.contact}
                 onChange={handleInputChange}
-                className="w-full bg-[#ece9e99c] px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
+                className="w-full bg-[#ece9e99c] px-4 py-[14px] border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
               />
-              {errors.contact && (
-                <p className="text-red-500 text-sm">{errors.contact}</p>
-              )}
             </div>
             <div>
               <input
@@ -133,18 +134,15 @@ const BasicDetails = () => {
                 placeholder="Email Address"
                 value={formData.email}
                 onChange={handleInputChange}
-                className="w-full bg-[#ece9e99c] px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
+                className="w-full bg-[#ece9e99c] px-4 py-[14px] border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
               />
-              {errors.email && (
-                <p className="text-red-500 text-sm">{errors.email}</p>
-              )}
             </div>
             <div>
               <select
                 id="location"
                 value={formData.location}
                 onChange={handleInputChange}
-                className="w-full bg-[#ece9e99c] px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
+                className="w-full bg-[#ece9e99c] px-4 py-[14px] border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-500"
               >
                 <option value="">Location</option>
                 <option value="newyork">New York</option>
@@ -153,22 +151,40 @@ const BasicDetails = () => {
                 <option value="houston">Houston</option>
                 <option value="miami">Miami</option>
               </select>
-              {errors.location && (
-                <p className="text-red-500 text-sm">{errors.location}</p>
-              )}
             </div>
           </>
         );
       case 2:
-        return <p>Education Details Form</p>;
+      case 2:
+        return (
+          <>
+            <EducationDetails />
+          </>
+        );
       case 3:
-        return <p>Work Experience Form</p>;
+        return (
+          <>
+            <WorkExp />
+          </>
+        );
       case 4:
-        return <p>Certification Form</p>;
+        return (
+          <>
+            <Certify />
+          </>
+        );
       case 5:
-        return <p>Skills and Preferences Form</p>;
+        return (
+          <>
+            <UploadDoc />
+          </>
+        );
       case 6:
-        return <p>Documents Form</p>;
+        return (
+          <>
+            <EducationDetails />
+          </>
+        );
       default:
         return null;
     }
@@ -176,126 +192,140 @@ const BasicDetails = () => {
 
   return (
     <div
-      className="flex flex-col md:flex-row min-h-screen bg-cover bg-center"
+      className="flex flex-col md:flex-row h-screen bg-cover bg-center overflow-y-hidden"
       style={{
         backgroundImage: `url(${LoginBack})`
       }}
     >
       <div className="hidden md:flex md:w-1/2 h-full flex-col items-center">
-      <div>
-      {/* For screens smaller than 1400px (Vertical Steps) */}
-      <div className="flex flex-col items-start gap-[35px] mt-6 relative xl:hidden">
-  {steps.map((step, index) => (
-    <div className="relative flex items-center space-x-4 bottom-[-15px]" key={index}>
-      {/* Circle */}
-      <div
-        className={`w-10 h-10 flex items-center justify-center rounded-full border-2 font-bold ${
-          currentStep > index + 1
-            ? "border-violet-700 bg-violet-700 text-white"
-            : currentStep === index + 1
-            ? "border-violet-700 bg-violet-700 text-white"
-            : "border-gray-300 bg-gray-100 text-gray-500"
-        }`}
-      >
-        {currentStep > index + 1 ? (
-          <span className="text-white">✓</span> // Checkmark for completed steps
-        ) : currentStep === index + 1 ? (
-          <span className="text-white">•</span> // Dot for current step
-        ) : null} 
-        {/* // No number for uncompleted steps */}
-      </div>
+        <div>
+          {/* For screens smaller than 1400px (Vertical Steps) */}
+          <div className="flex flex-col items-start gap-[35px] mt-6 relative xl:hidden">
+            {steps.map((step, index) => (
+              <div
+                className="relative flex items-center space-x-4 bottom-[-15px]"
+                key={index}
+              >
+                {/* Circle */}
+                <div
+                  className={`w-10 h-10 flex items-center justify-center rounded-full border-2 font-bold ${
+                    currentStep > index + 1
+                      ? "border-violet-700 bg-violet-700 text-white"
+                      : currentStep === index + 1
+                      ? "border-violet-700 bg-violet-700 text-white"
+                      : "border-gray-300 bg-gray-100 text-gray-500"
+                  }`}
+                >
+                  {currentStep > index + 1 ? (
+                    <span className="text-white">✓</span> // Checkmark for completed steps
+                  ) : currentStep === index + 1 ? (
+                    <span className="text-white">•</span> // Dot for current step
+                  ) : null}
+                  {/* // No number for uncompleted steps */}
+                </div>
 
-      {/* Step Name */}
-      <span
-        className={`text-sm ${
-          currentStep >= index + 1 ? "text-violet-700" : "text-gray-500"
-        }`}
-      >
-        {step}
-      </span>
+                {/* Step Name */}
+                <span
+                  className={`text-sm ${
+                    currentStep >= index + 1
+                      ? "text-violet-700"
+                      : "text-gray-500"
+                  }`}
+                >
+                  {step}
+                </span>
 
-      {/* Vertical Line */}
-      {index < steps.length - 1 && (
-        <div className="absolute z-[-1] left-[4px] top-[25px] w-[2px] h-[60px] bg-gray-300"></div>
-      )}
-    </div>
-  ))}
-</div>
+                {/* Vertical Line */}
+                {index < steps.length - 1 && (
+                  <div className="absolute z-[-1] left-[4px] top-[25px] w-[2px] h-[60px] bg-gray-300"></div>
+                )}
+              </div>
+            ))}
+          </div>
 
-      {/* For screens larger than 1400px (Horizontal Steps) */}
-<div className="hidden xl:flex justify-center items-center mb-4 mt-[3rem]">
-  {steps.map((step, index) => (
-    <div key={index} className="relative flex items-center">
-      {/* Circle */}
-      <div
-        className={`w-8 h-8 flex items-center justify-center rounded-full font-bold ${
-          currentStep > index + 1
-            ? "bg-violet-700 text-white"
-            : currentStep === index + 1
-            ? "bg-violet-700 text-violet-700"
-            : "bg-gray-300 text-gray-500"
-        }`}
-      >
-        {currentStep > index + 1 ? (
-          <span className="text-white">✓</span> // Checkmark for completed steps
-        ) : currentStep === index + 1 ? (
-          <span className="text-white">•</span> // Dot for the current step
-        ) : null} 
-        {/* // No number for future steps */}
-      </div>
+          {/* For screens larger than 1400px (Horizontal Steps) */}
+          <div className="hidden xl:flex justify-center items-center mb-4 mt-[3rem]">
+            {steps.map((step, index) => (
+              <div key={index} className="relative flex items-center">
+                {/* Circle */}
+                <div
+                  className={`w-[35px] h-[35px] 2xl:w-[3rem] 2xl:h-[3rem] flex items-center justify-center rounded-full font-bold ${
+                    currentStep > index + 1
+                      ? "bg-violet-700 text-white"
+                      : currentStep === index + 1
+                      ? "bg-violet-700 text-violet-700"
+                      : "bg-gray-300 text-gray-500"
+                  }`}
+                >
+                  {currentStep > index + 1 ? (
+                    <span className="text-white">✓</span> // Checkmark for completed steps
+                  ) : currentStep === index + 1 ? (
+                    <span className="text-white">•</span> // Dot for the current step
+                  ) : null}
+                  {/* // No number for future steps */}
+                </div>
 
-      {/* Line Between Circles */}
-      {index < steps.length - 1 && (
-        <div
-          className={`h-[3px] w-20 ${
-            currentStep > index + 1 ? "bg-violet-700" : "bg-gray-300"
-          }`}
-        ></div>
-      )}
+                {/* Line Between Circles */}
+                {index < steps.length - 1 && (
+                  <div
+                    className={`h-[3px] md:w-[4rem] ${
+                      currentStep > index + 1 ? "bg-violet-700" : "bg-gray-300"
+                    }`}
+                  ></div>
+                )}
 
-      {/* Step Name Below Circle */}
-      <div className="absolute bottom-[-25px] left-[-25px] text-center w-full">
-        <span
-          className={`text-xs ${
-            currentStep === index + 1
-              ? "text-violet-700 font-semibold"
-              : "text-gray-600"
-          }`}
-        >
-          {step}
-        </span>
-      </div>
-    </div>
-  ))}
-</div>
-
-
-    </div>
-    <img
+                {/* Step Name Below Circle */}
+                <div className="absolute bottom-[-25px] left-[-25px] text-center w-full">
+                  <span
+                    className={`text-xs ${
+                      currentStep >= index + 1
+                        ? "text-violet-700 font-semibold"
+                        : "text-gray-600"
+                    }`}
+                  >
+                    {step}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <img
           src={logos[currentStep - 1]}
           alt="Step Logo"
-          className="lg:w-[70%] md:w-[75%] h-auto xl:mt-[12rem] lg:mt-[4rem] md:mt-[7rem]"
+          className="2xl:w-[55%] xl:w-[65%] lg:w-[65%] md:w-[75%] h-auto xl:mt-[12rem] xl:mt-[10rem] lg:mt-[4rem] md:mt-[7rem]"
         />
       </div>
-      <div className="bg-white rounded-tl-[0px] rounded-bl-[0px] shadow-lg p-8 w-full max-w-3xl sm:rounded-tl-[80px] sm:rounded-bl-[80px]">
-        <h1 className="text-[32px] font-semibold text-gray-800 text-center mb-4">
-          {steps[currentStep - 1]}
-        </h1>
-        <p className="text-center text-gray-500 mb-8">
-          Let’s Start with the Basics
-        </p>
-        <form onSubmit={handleSubmit} className="space-y-8 w-[80%] m-auto">
-          {renderFormContent()}
-          <div className="text-center">
-            <button
-              type="submit"
-              className="flex justify-center items-center w-full bg-violet-700 text-white py-3 rounded-lg font-semibold hover:bg-violet-800 focus:outline-none focus:ring-2 focus:ring-violet-500"
-            >
-              Save and Continue
-              <AiOutlineArrowRight className="mx-3 " />
-            </button>
-          </div>
-        </form>
+      <div
+        className="bg-white rounded-tl-[0px] rounded-bl-[0px] shadow-lg p-8 w-full ml-auto lg:max-w-[45rem] 2xl:max-w-[55rem] sm:rounded-tl-[80px] sm:rounded-bl-[80px]"
+        style={{ height: "calc(103vh - 20px)" }}
+      >
+        <div>
+          <h1 className="text-[32px] font-semibold text-gray-800 text-center mb-4">
+            {formsName[currentStep - 1]}
+          </h1>
+          <p className="text-center text-gray-500 mb-8">
+            {subhead[currentStep - 1]}
+          </p>
+        </div>
+
+        <div
+          className="overflow-y-auto"
+          style={{ maxHeight: "calc(100vh - 150px)" }} // Adjust height based on your layout
+        >
+          <form onSubmit={handleSubmit} className="space-y-6 w-[80%] m-auto">
+            {renderFormContent()}
+            <div className="text-center pb-[13px]">
+              <button
+                type="submit"
+                className="py-[1rem] flex justify-center items-center w-full bg-violet-700 text-white py-3 rounded-lg font-semibold hover:bg-violet-800 focus:outline-none focus:ring-2 focus:ring-violet-500"
+              >
+                Save and Continue
+                <AiOutlineArrowRight className="mx-3 " />
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/modules/BasicDetails/skillsPref.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const skillsPref = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default skillsPref
