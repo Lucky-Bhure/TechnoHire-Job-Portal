99e4601a5ab8589fd629eb380112361b9f76cedc
Krunal-kandoliya <139053154+Krunal-kandoliya@users.noreply.github.com>
2024-12-10 10:15:25 +0530

    findajob page done

--- src/pages/modules/Overview/Overview.jsx
+++ src/pages/modules/Overview/Overview.jsx
@@ -84,57 +84,55 @@ const Overview = () => {
         </div>
 
         <div
-  style={{ backgroundColor: theme.bgColorforProfile }}
-  className="grid grid-cols-1 sm:grid-cols-3 mt-6 rounded-lg overflow-hidden"
->
-  <div className="col-span-3 px-4 sm:px-10 py-7 flex flex-col sm:flex-row items-center sm:items-start">
-    <img
-      className="cursor-pointer w-[70px] h-[70px] object-cover rounded-full hover:border-2 hover:border-black mb-4 sm:mb-0"
-      src="https://plus.unsplash.com/premium_photo-1690086519096-0594592709d3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      alt="Profile"
-    />
-    <div className="mt-4 sm:mt-0 sm:ml-10 text-center sm:text-left">
-      <p className="font-semibold text-lg sm:text-xl">
-        Your Profile editing is not completed
-      </p>
-      <p className="text-sm sm:text-base">Complete your profile</p>
-    </div>
-    <div className="ml-auto flex items-center justify-center sm:justify-start mt-4 sm:mt-0">
-      <Flex wrap gap="small">
-        <Progress
-          strokeColor={theme.progressBarcolor}
-          type="circle"
-          percent={90}
-          size={80}
-        />
-        <div className="relative right-[78px] top-[10px]">
-          <Progress
-            type="circle"
-            strokeColor={theme.progressBarcolor}
-            percent={90}
-            format={() => null}
-            size={60}
-          />
-        </div>
-      </Flex>
-    </div>
-    <button className="bg-white cursor-pointer my-2 h-[55px] w-[165px] mx-auto sm:mx-0">
-      <div className="px-4 py-[17px]">
-        <p
-          className="font-bold "
-          style={{ color: theme.ButtonbgColor }}
+          style={{ backgroundColor: theme.bgColorforProfile }}
+          className="grid grid-cols-1 sm:grid-cols-3 mt-6 rounded-lg overflow-hidden"
         >
-          Edit Profile
-        </p>
-        <div className="relative left-28 bottom-5 sm:left-28 sm:bottom-5">
-          <FaArrowRight color={theme.ButtonbgColor} />
+          <div className="col-span-3 px-4 sm:px-10 py-7 flex flex-col sm:flex-row items-center sm:items-start">
+            <img
+              className="cursor-pointer w-[70px] h-[70px] object-cover rounded-full hover:border-2 hover:border-black mb-4 sm:mb-0"
+              src="https://plus.unsplash.com/premium_photo-1690086519096-0594592709d3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Profile"
+            />
+            <div className="mt-4 sm:mt-0 sm:ml-10 text-center sm:text-left">
+              <p className="font-semibold text-lg sm:text-xl">
+                Your Profile editing is not completed
+              </p>
+              <p className="text-sm sm:text-base">Complete your profile</p>
+            </div>
+            <div className="ml-auto flex items-center justify-center sm:justify-start mt-4 sm:mt-0">
+              <Flex wrap gap="small">
+                <Progress
+                  strokeColor={theme.progressBarcolor}
+                  type="circle"
+                  percent={90}
+                  size={80}
+                />
+                <div className="relative right-[78px] top-[10px]">
+                  <Progress
+                    type="circle"
+                    strokeColor={theme.progressBarcolor}
+                    percent={90}
+                    format={() => null}
+                    size={60}
+                  />
+                </div>
+              </Flex>
+            </div>
+            <button className="bg-white cursor-pointer my-2 h-[55px] w-[165px] mx-auto sm:mx-0">
+              <div className="px-4 py-[17px]">
+                <p
+                  className="font-bold "
+                  style={{ color: theme.ButtonbgColor }}
+                >
+                  Edit Profile
+                </p>
+                <div className="relative left-28 bottom-5 sm:left-28 sm:bottom-5">
+                  <FaArrowRight color={theme.ButtonbgColor} />
+                </div>
+              </div>
+            </button>
+          </div>
         </div>
-      </div>
-    </button>
-  </div>
-</div>
-
-        
 
         <div className="mt-6 mb-4 flex justify-between items-center">
           <p className="text-lg font-semibold">Recently Applied</p>
@@ -145,59 +143,61 @@ const Overview = () => {
         </div>
 
         <div className="overflow-x-auto bg-white shadow-md rounded-lg">
-        <table className="table-auto w-full">
-  <thead>
-    <tr style={{ backgroundColor: "#F1F2F4" }} className="text-center">
-      <th className="px-6 py-3 text-sm">Jobs</th>
-      <th className="px-6 py-3 text-sm">Date Applied</th>
-      <th className="px-6 py-3 text-sm">Status</th>
-      <th className="px-6 py-3 text-sm">Action</th>
-    </tr>
-  </thead>
-  <tbody className="animate-fade-in">
-    {jobData.map((e) => (
-      <tr className="border-b-2 border-gray-300" key={e.id}>
-        <td className="px-4 py-2 font-medium text-center text-gray-500">
-          <div className="flex items-center">
-            <img src={apple} width={50} alt="Company Logo" />
-            <p className="ml-3 text-sm">{e.title}</p>
-            <span
-              style={{ backgroundColor: "#DCBDED" }}
-              className="py-1 px-2 ml-2 h-6 text-xs rounded-xl"
-            >
-              <span style={{ color: theme.ButtonbgColor }}>
-                {e.type}
-              </span>
-            </span>
-          </div>
-          <div className="flex items-center mt-2">
-            <CiLocationOn className="text-gray-500 mr-2" size={20} />
-            <p className="text-sm">{e.location}</p>
-            <p className="ml-4 text-sm">{e.salary}</p>
-          </div>
-        </td>
+          <table className="table-auto w-full">
+            <thead>
+              <tr
+                style={{ backgroundColor: "#F1F2F4" }}
+                className="text-center"
+              >
+                <th className="px-6 py-3 text-sm">Jobs</th>
+                <th className="px-6 py-3 text-sm">Date Applied</th>
+                <th className="px-6 py-3 text-sm">Status</th>
+                <th className="px-6 py-3 text-sm">Action</th>
+              </tr>
+            </thead>
+            <tbody className="animate-fade-in">
+              {jobData.map((e) => (
+                <tr className="border-b-2 border-gray-300" key={e.id}>
+                  <td className="px-4 py-2 font-medium text-center text-gray-500">
+                    <div className="flex items-center">
+                      <img src={apple} width={50} alt="Company Logo" />
+                      <p className="ml-3 text-sm">{e.title}</p>
+                      <span
+                        style={{ backgroundColor: "#DCBDED" }}
+                        className="py-1 px-2 ml-2 h-6 text-xs rounded-xl"
+                      >
+                        <span style={{ color: theme.ButtonbgColor }}>
+                          {e.type}
+                        </span>
+                      </span>
+                    </div>
+                    <div className="flex items-center ms-14">
+                      <CiLocationOn className="text-gray-500 mr-2" size={20} />
+                      <p className="text-sm">{e.location}</p>
+                      <p className="ml-4 text-sm">{e.salary}</p>
+                    </div>
+                  </td>
 
-        <td className="px-4 py-2 font-medium text-center text-gray-500">
-          {e.dateApplied}
-        </td>
+                  <td className="px-4 py-2 font-medium text-center text-gray-500">
+                    {e.dateApplied}
+                  </td>
 
-        <td className="px-4 py-2 font-medium text-center text-green-500">
-          <div className="flex items-center">
-            <TiTick color="green" />
-            <p className="ml-2">{e.status}</p>
-          </div>
-        </td>
+                  <td className="px-4 py-2 font-medium text-center text-green-500">
+                    <div className="flex items-center">
+                      <TiTick color="green" />
+                      <p className="ml-2">{e.status}</p>
+                    </div>
+                  </td>
 
-        <td className="px-4 py-2 text-center">
-          <button className="bg-[#DCBDED] hover:bg-[#7900BA] hover:text-white text-[#7900BA] text-sm px-4 py-2 rounded-lg">
-            View Details
-          </button>
-        </td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-
+                  <td className="px-4 py-2 text-center">
+                    <button className="bg-[#DCBDED] hover:bg-[#7900BA] hover:text-white text-[#7900BA] text-sm px-4 py-2 rounded-lg">
+                      View Details
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </Layout>
