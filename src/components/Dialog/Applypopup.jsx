a1de86df9a9070e37ef5d076b11bbb1686a84a1e
Dilip Kumar Yadav <yadavdip32@gmail.com>
2024-12-14 10:57:06 +0530

    update-findjob

--- src/components/Dialog/Applypopup.jsx
+++ src/components/Dialog/Applypopup.jsx
@@ -54,9 +54,9 @@ const Applypopup = ({ setshowmodal }) => {
   }
 
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed  inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
       {/* Popup Card */}
-      <div className="bg-white rounded-lg  m-9 shadow-lg w-[708px] max-w-lg p-6">
+      <div className="bg-white  rounded-lg  m-9 shadow-lg w-[708px] max-w-lg p-6">
         {/* Header */}
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-lg font-bold">Apply Job: Senior UX Designer</h2>
