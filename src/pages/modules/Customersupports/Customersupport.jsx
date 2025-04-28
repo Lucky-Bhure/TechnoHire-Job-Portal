495d9249657357a30295aa455f9d2fd890632c23
Lucky-Bhure <luckybhure19@gmail.com>
2025-04-28 12:53:38 +0530

    Customersupport.jsx

--- src/pages/modules/Customersupports/Customersupport.jsx
+++ src/pages/modules/Customersupports/Customersupport.jsx
@@ -1,7 +1,6 @@
-import MainHeader from "../../../components/header/MainHeader";
-import Navbar from "../../../components/header/Header";
-import Sidebar from "../../../components/sidebar/Sidebar";
 import React from "react";
+import Sidebar from "../../../components/Sidebar/Sidebar";
+import Navbar from "../../../components/header/Navbar";
 
 const Customersupport = () => {
   return (
