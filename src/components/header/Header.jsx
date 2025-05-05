59485127d6a0730209ccecd5e9fc4820cd2bf5a2
harsh-barman <harshbarman015@gmail.com>
2025-05-05 17:37:35 +0530

    search bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,26 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Input, Select, Space } from "antd";
-import logo2 from '../../assets/logo2.png'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Select, Space } from "antd";
+import logo2 from '../../assets/logo2.png';
 import MainHeader from "./MainHeader";
 import { TbMessage2 } from "react-icons/tb";
 import { PiBellRingingBold } from "react-icons/pi";
-// import Notification from "../../pages/modules/Notification/Notification";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
-
   const navigate = useNavigate();
+
   const handleMessages = () => {
     navigate("/Inboxes");
-  }
-  const handelNotification =()=>{
+  };
+
+  const handleNotification = () => {
     navigate("/notification");
-  }
+  };
+
   const handleUser = () => {
-    navigate('/my-profile')
-  }
+    navigate('/my-profile');
+  };
 
   const options = [
     {
@@ -29,8 +31,7 @@ const Header = () => {
           India
         </div>
       ),
-    }
-    ,
+    },
     {
       value: "Germany",
       label: (
@@ -43,56 +44,57 @@ const Header = () => {
           Germany
         </div>
       ),
-    }
-    ,
+    },
   ];
 
   return (
     <>
-      <div className="w-full ">
+      <div className="w-full">
         <MainHeader />
         <div className="w-full h-auto bg-white px-4 sm:px-8">
           <div className="flex flex-wrap justify-between items-center">
             {/* Logo Section */}
             <div className="flex items-center w-full sm:w-auto mb-4 sm:mb-0 gap-10">
-              <div className="flex items-center py-">
-                <img src={logo2} alt="Logo" className="w-[55px] h-[60px] object-contain" />
-                <div className="w-fit pr-1">
-                  <p className=" text-2xl sm:text-2xl">TechnoHire</p>
+              <div className="flex items-center">
+                <img src={logo2} alt="Logo" className="w-[55px] h-[65px] object-contain mt-2" />
+                <div className="w-fit pr-2 mt-4">
+                  <p className="font-[500] text-[25px]">TechnoHire</p>
                   <p className="text-[8px] font-bold text-white bg-[#8b008d] px-2 py-1">
                     Where Talent Meets Opportunity{" "}
                   </p>
                 </div>
               </div>
-
-              <div className=" w-[650px] my-2 justify-center">
-              <Space.Compact className="w-[750px] py-4 rounded-none h-[50px] ">
-                <Select
-                  options={options}
-                  defaultValue={options[0]?.value}
-                  style={{ height: "50px" }}
-                  className=" w-[300px]"
-                />
-                <Input
-                  className="h-[50px]  rounded-none "
-                  placeholder="Job title,keyword,company"
-                />
-              </Space.Compact>
+              {/* Search Section */}
+              <div className="w-[650px] my-2 justify-center">
+                <Space.Compact className="w-[750px] py-4 rounded-none h-[50px]">
+                  <Select
+                    options={options}
+                    defaultValue={options[0]?.value}
+                    style={{ height: "50px" }}
+                    className="w-[300px]"
+                  />
+                  <div className="relative flex items-center justify-start w-full h-[50px]">
+                    <input
+                      type="text"
+                      className="pl-10 pr-4 w-full border border-gray-300 rounded-none outline-none h-full flex items-center"
+                      placeholder="Job title, keyword, company"
+                    />
+                    <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-purple-600 text-lg" />
+                  </div>
+                </Space.Compact>
+              </div>
             </div>
-            </div>
-
-            {/* Search Section */}
-            
 
             {/* User Section */}
             <div className="flex items-center gap-4 w-full sm:w-auto justify-center sm:justify-end">
-             <TbMessage2 onClick={handleMessages} className="w-6 h-6 text-gray-600 cursor-pointer"/>  
-              <div className="relative"> 
-                <PiBellRingingBold onClick={handelNotification} className='w-6 h-6 text-gray-600 cursor-pointer'/>
+              <TbMessage2 onClick={handleMessages} className="w-6 h-6 text-gray-600 cursor-pointer" />
+              <div className="relative">
+                <PiBellRingingBold onClick={handleNotification} className="w-6 h-6 text-gray-600 cursor-pointer" />
                 <span className="absolute top-0 right-0 w-2.5 h-2.5 bg-red-500 rounded-full"></span>
               </div>
               <div className="relative">
-                <img onClick={handleUser}
+                <img
+                  onClick={handleUser}
                   className="w-8 h-8 object-cover rounded-full cursor-pointer hover:border-2 hover:border-black"
                   src="https://plus.unsplash.com/premium_photo-1690086519096-0594592709d3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="User Avatar"
@@ -103,11 +105,7 @@ const Header = () => {
           </div>
         </div>
         <div className="border-t-2 border-gray-300 mt-4"></div>
-
-
       </div>
-
-
     </>
   );
 };
